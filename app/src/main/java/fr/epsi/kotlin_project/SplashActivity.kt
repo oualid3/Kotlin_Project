2c00e6169b3e053a3e5e6036d767566f0afd320d
oualid3 <73546426+oualid3@users.noreply.github.com>
2022-01-24 15:03:23 +0100

    splash

---
+++ app/src/main/java/fr/epsi/kotlin_project/SplashActivity.kt
@@ -0,0 +1,18 @@
+package fr.epsi.kotlin_project
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            val newIntent=Intent(application,MainActivity::class.java)
+            startActivity(newIntent)
+            finish()
+        },2000)
+    }
+}
