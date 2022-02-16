cf15ca2b78ad775630dcb6d6498832c6f33a3973
oualid3 <73546426+oualid3@users.noreply.github.com>
2022-02-16 10:36:55 +0100

    Main

--- app/src/main/java/fr/epsi/kotlin_project/MainActivity.kt
+++ app/src/main/java/fr/epsi/kotlin_project/MainActivity.kt
@@ -2,10 +2,29 @@ package fr.epsi.kotlin_project
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonZone1: Button = findViewById(R.id.buttonZone1)
+        val buttonZone2: Button = findViewById(R.id.buttonZone2)
+
+//        buttonZone1.setOnClickListener(View.OnClickListener {
+//            val newIntent= Intent(application,StudentsActivity::class.java)
+//            newIntent.putExtra("title",getString(R.string.zone_1))
+//            startActivity(newIntent)
+//        })
+//
+//        buttonZone2.setOnClickListener(View.OnClickListener {
+//            val newIntent= Intent(application,ShopActivity::class.java)
+//            newIntent.putExtra("title",getString(R.string.zone_2))
+//            startActivity(newIntent)
+//        })
+
     }
+
+
 }
