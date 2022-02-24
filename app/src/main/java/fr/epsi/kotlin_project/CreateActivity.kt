81dfebafc55f4ffe15692478ff8541ae63beabaf
BenjaminEmm <benjamin.emmanuel@epsi.fr>
2022-02-24 18:30:28 +0100

    Dépendances Strings CreateActivity QrCodeActivity

--- app/src/main/java/fr/epsi/kotlin_project/CreateActivity.kt
+++ app/src/main/java/fr/epsi/kotlin_project/CreateActivity.kt
@@ -1,12 +1,89 @@
 package fr.epsi.kotlin_project
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class CreateActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
         showBtnBack()
+
+        val lastName = intent.getStringExtra("lastName")
+        val editLastName = findViewById<EditText>(R.id.editTextLastName)
+
+        val firstName = intent.getStringExtra("firstName")
+        val editFirstName = findViewById<EditText>(R.id.editTextFirstName)
+
+        val email = intent.getStringExtra("email")
+        val editEmail = findViewById<EditText>(R.id.editTextEmailAddress)
+
+        val address = intent.getStringExtra("address")
+        val editAddress = findViewById<EditText>(R.id.editTextAddress)
+
+        val zipcode = intent.getStringExtra("zipcode")
+        val editZipcode = findViewById<EditText>(R.id.editTextZipcode)
+
+        val city = intent.getStringExtra("city")
+        val editCity = findViewById<EditText>(R.id.editTextCity)
+
+        val cardRef = intent.getStringExtra("cardRef")
+        val editCardRef = findViewById<EditText>(R.id.editTextLoyaltyCard)
+
+        val button = findViewById<Button>(R.id.buttonLogin)
+
+
+        button.setOnClickListener(View.OnClickListener{
+
+            val newIntent = Intent(application,FragmentActivity::class.java)
+            writeSharedPref("firstName",editFirstName.text.toString())
+            writeSharedPref("lastName",editLastName.text.toString())
+            writeSharedPref("email",editEmail.text.toString())
+            writeSharedPref("address",editAddress.text.toString())
+            writeSharedPref("zipcode",editZipcode.text.toString())
+            writeSharedPref("city",editCity.text.toString())
+            writeSharedPref("cardRef",editCardRef.text.toString())
+            writeSharedisCreated("isCreated",true)
+            startActivity(newIntent)
+        })
+
+
+        if(!(firstName.equals(null)))
+        {
+            editFirstName.setText(firstName)
+            editLastName.setText(lastName)
+            editEmail.setText(email)
+            editAddress.setText(address)
+            editZipcode.setText(zipcode)
+            editCity.setText(city)
+            editCardRef.setText(cardRef)
+        }
+    }
+
+
+    fun writeSharedPref(key:String,value:String){
+        val sharedPreferences: SharedPreferences = getSharedPreferences("account", Context.MODE_PRIVATE)
+        val editor =sharedPreferences.edit()
+        editor.putString(key,value)
+        editor.apply()
+    }
+    fun writeSharedisCreated(key:String,value:Boolean){
+        val sharedPreferences: SharedPreferences = getSharedPreferences("account", Context.MODE_PRIVATE)
+        val editor =sharedPreferences.edit()
+        editor.putBoolean(key,value)
+        editor.apply()
+    }
+
+
+    fun readSharedPref(key:String):String{
+        val sharedPreferences: SharedPreferences = getSharedPreferences("account", Context.MODE_PRIVATE)
+        return sharedPreferences.getString(key,"not found").toString()
     }
 }
--- app/src/main/java/fr/epsi/kotlin_project/MainActivity.kt
+++ app/src/main/java/fr/epsi/kotlin_project/MainActivity.kt
@@ -1,15 +1,23 @@
 package fr.epsi.kotlin_project
 
+import android.content.Context
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val sharedPreferences: SharedPreferences = getSharedPreferences("account", Context.MODE_PRIVATE)
+        if(sharedPreferences.getBoolean("isCreated",false)){
+            val newIntent = Intent(application,FragmentActivity::class.java)
+            startActivity(newIntent)
+        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val buttonZone1: Button = findViewById(R.id.buttonZone1)
         val buttonZone2: Button = findViewById(R.id.buttonZone2)
 
@@ -18,12 +26,12 @@ class MainActivity : AppCompatActivity() {
             newIntent.putExtra("title",getString(R.string.zone_2))
             startActivity(newIntent)
         })
-//
-//        buttonZone2.setOnClickListener(View.OnClickListener {
-//            val newIntent= Intent(application,ShopActivity::class.java)
-//            newIntent.putExtra("title",getString(R.string.zone_2))
-//            startActivity(newIntent)
-//        })
+
+        buttonZone1.setOnClickListener(View.OnClickListener {
+            val newIntent= Intent(application,QrcodeActivity::class.java)
+            newIntent.putExtra("title",getString(R.string.zone_1))
+            startActivity(newIntent)
+        })
 
     }
 
