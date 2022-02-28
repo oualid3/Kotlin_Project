d1d731b8afde9ff5bf41d6c8c6abdee5207c3c2d
BenjaminEmm <benjamin.emmanuel@epsi.fr>
2022-02-28 18:38:53 +0100

    Add offer class, offer adapter and tab for offers in bar tab

--- app/src/main/java/fr/epsi/kotlin_project/FragmentActivity.kt
+++ app/src/main/java/fr/epsi/kotlin_project/FragmentActivity.kt
@@ -10,21 +10,28 @@ import androidx.fragment.app.Fragment
 class FragmentActivity : BaseActivity() {
 
     private lateinit var tab1Fragment : Fragment;
+    private lateinit var tab2Fragment : Fragment;
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment)
         tab1Fragment = Tab1Fragment.newInstance();
+        tab2Fragment = Tab2Fragment.newInstance("Hey","Hello");
 
 
         val tab1=findViewById<TextView>(R.id.textViewTab1)
+        val tab2=findViewById<TextView>(R.id.textViewTab2)
 
         showBtnBack()
         setLogoHeader()
 
         tab1.setOnClickListener(View.OnClickListener {
             showTab1()
+        })
+
+        tab2.setOnClickListener(View.OnClickListener {
+            showTab2()
         })
 
         showTab1()
@@ -37,6 +44,15 @@ class FragmentActivity : BaseActivity() {
         fragmentTransaction.setReorderingAllowed(true)
         fragmentTransaction.addToBackStack("Tab1Fragment") // name can be null
         fragmentTransaction.replace(R.id.fragment_container, tab1Fragment, null)
+        fragmentTransaction.commit()
+    }
+
+    private fun showTab2() {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.setReorderingAllowed(true)
+        fragmentTransaction.addToBackStack("Tab2Fragment") // name can be null
+        fragmentTransaction.replace(R.id.fragment_container, tab2Fragment, null)
         fragmentTransaction.commit()
     }
 
