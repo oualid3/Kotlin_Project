d60309f8df8121c0f829dec6a5d37f3a29b89b92
BenjaminEmm <benjamin.emmanuel@epsi.fr>
2022-03-03 22:41:05 +0100

    add store activity

---
+++ app/src/main/java/fr/epsi/kotlin_project/StoreActivity.kt
@@ -0,0 +1,16 @@
+package fr.epsi.kotlin_project
+
+import android.media.Image
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+
+class StoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_store)
+
+    }
+
+}
